b84da62c571479d142471dfe77f874ef39b213de
Julian McNeill <julesdowork@gmail.com>
2019-12-28 21:25:30 -0500

    Preparing Map Generator

---
+++ Awesome Road Runner/Assets/Scripts/Helper Scripts/MapGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public static MapGenerator instance;
+
+    public GameObject
+        roadPrefab,
+        grassPrefab,
+        groundPrefab1,
+        groundPrefab2,
+        groundPrefab3,
+        groundPrefab4,
+        grassBottomPrefab,
+        landPrefab1,
+        landPrefab2,
+        landPrefab3,
+        landPrefab4,
+        landPrefab5,
+        bigGrassPrefab,
+        bigGrassBottomPrefab,
+        treePrefab1,
+        treePrefab2,
+        treePrefab3,
+        bigTreePrefab;
+
+    public GameObject
+        roadHolder,
+        topNearSidewalkHolder,
+        topFarSidewalkHolder,
+        bottomNearSidewalkHolder,
+        bottomFarSidewalkHolder;
+
+    public List<GameObject>
+        roadTiles,
+        topNearGrassTiles,
+        topFarGrassTiles,
+        bottomNearGrassTiles,
+        bottomFarLandF1Tiles,
+        bottomFarLandF2Tiles,
+        bottomFarLandF3Tiles,
+        bottomFarLandF4Tiles,
+        bottomFarLandF5Tiles;
+    [HideInInspector]
+    public Vector3
+        lastPosOfRoad,
+        lastPosOfTopNearGrass,
+        lastPosOfTopFarGrass,
+        lastPosOfBottomNearGrass,
+        lastPosOfBottomFarLandF1,
+        lastPosOfBottomFarLandF2,
+        lastPosOfBottomFarLandF3,
+        lastPosOfBottomFarLandF4,
+        lastPosOfBottomFarLandF5;
+
+    public int
+        startRoadTile,      // initialization: number of "road" tiles
+        startGrassTile,     // initialization: number of "grass" tiles
+        startGround3Tile,   // initialization: number of "ground3" tiles
+        startLandTile;      // initialization: number of "land" tiles
+
+    [Header("Positions to Ground Tile")]
+    public int[] posForTopGround1;      // positions for ground1 on top from 0 to startGround3Tile
+    public int[] posForTopGround2;      // positions for ground2 on top from 0 to startGround3Tile
+    public int[] posForTopGround4;      // positions for ground4 on top from 0 to startGround3Tile
+
+    [Header("Positions to Grass Tile")]
+    public int[] posForTopBigGrass;     // positions for big grass with tree on top near grass from 0 to startGrassTile
+    public int[] posForTopTree1;        // positions for tree1 on top near grass from 0 to startGrassTile
+    public int[] posForTopTree2;        // positions for tree1 on top near grass from 0 to startGrassTile
+    public int[] posForTopTree3;        // positions for tree1 on top near grass from 0 to startGrassTile
+    public int[] posForBottomBigGrass;       // positions for big grass with tree on bottom near grass from 0 to startGrassTile
+    public int[] posForBottomTree1;     // positions for tree1 on bottom near grass from 0 to startGrassTile
+    public int[] posForBottomTree2;     // positions for tree2 on bottom near grass from 0 to startGrassTile
+    public int[] posForBottomTree3;     // positions for tree3 on bottom near grass from 0 to startGrassTile
+
+    [Header("Positions to Road Tile")]
+    public int posForRoadTile1;       // position for road tile on road from 0 to startRoadTile
+    public int posForRoadTile2;       // position for road tile on road from 0 to startRoadTile
+    public int posForRoadTile3;       // position for road tile on road from 0 to startRoadTile
+    [HideInInspector]
+    public int
+        lastOrderOfRoad,
+        lastOrderOfTopNearGrass,
+        lastOrderOfTopFarGrass,
+        lastOrderOfBottomNearGrass,
+        lastOrderOfBottomFarLandF1,
+        lastOrderOfBottomFarLandF2,
+        lastOrderOfBottomFarLandF3,
+        lastOrderOfBottomFarLandF4,
+        lastOrderOfBottomFarLandF5;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Awesome Road Runner/Assets/Scripts/Helper Scripts/MyTags.cs
@@ -0,0 +1,26 @@
+﻿public class MyTags
+{
+    // Tags
+    public static string ROAD = "Road";
+    public static string TOP_NEAR_GRASS = "TopNearGrass";
+    public static string TOP_FAR_GRASS = "TopFarGrass";
+    public static string BOTTOM_NEAR_GRASS = "BottomNearGrass";
+    public static string BOTTOM_FAR_LAND_1 = "BottomFarLand1";
+    public static string BOTTOM_FAR_LAND_2 = "BottomFarLand2";
+    public static string BOTTOM_FAR_LAND_3 = "BottomFarLand3";
+    public static string BOTTOM_FAR_LAND_4 = "BottomFarLand4";
+    public static string BOTTOM_FAR_LAND_5 = "BottomFarLand5";
+    public static string OBSTACLE = "Obstacle";
+    public static string STAR = "Star";
+    public static string STAR_EFFECT = "StarEffect";
+    public static string OB_TOP = "ObTop";
+    public static string OB_MIDDLE = "ObMiddle";
+    public static string OB_BOTTOM = "ObBottom";
+    public static string T_REX = "T-Rex";
+
+    // Sorting Layers
+    public static string SL_TOP_NEAR_GROUND = "Top Near Ground";
+    public static string SL_TREE_TOP = "Tree Top";
+    public static string SL_BOTTOM_NEAR_GROUND = "Bottom Near Ground";
+    public static string SL_TREE_BOTTOM = "Tree Bottom";
+}
