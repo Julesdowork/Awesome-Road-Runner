e000595c811b76f83c3553ff64e290f27d6243e6
Julian McNeill <julesdowork@gmail.com>
2019-12-30 18:41:37 -0500

    Obstacle Holder

---
+++ Awesome Road Runner/Assets/Scripts/Obstacle Scripts/ObstacleHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHolder : MonoBehaviour
+{
+    public GameObject[] children;
+    public Vector3 firstPos, secondPos;
+
+    public float limitAxisX;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-GameplayController.instance.moveSpeed * Time.deltaTime, 0, 0);
+
+        if (transform.localPosition.x <= limitAxisX)
+        {
+            // INFORM THE GAMEPLAY CONTROLLER THAT THE OBSTACLE IS NOT ACTIVE
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        for (int i = 0; i < children.Length; i++)
+        {
+            children[i].SetActive(true);
+        }
+
+        if (Random.value <= 0.5f)
+        {
+            transform.localPosition = firstPos;
+        }
+        else
+        {
+            transform.localPosition = secondPos;
+        }
+    }
+}
