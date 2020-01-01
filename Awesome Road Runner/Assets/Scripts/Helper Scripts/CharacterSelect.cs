d1d51d3552af1099824bfd99d2ef74436abc48c8
Julian McNeill <julesdowork@gmail.com>
2019-12-31 22:19:11 -0500

    Main Menu Controller

---
+++ Awesome Road Runner/Assets/Scripts/Helper Scripts/CharacterSelect.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public GameObject[] availableHeroes;
+    public Text selectedText;
+    public GameObject starIcon;
+    public Image selectBtnImage;
+    public Sprite buttonGreen, buttonBlue;
+    public Text starScoreText;
+
+    private int currentIndex;
+    private bool[] heroes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeCharacters();
+    }
+
+    private void InitializeCharacters()
+    {
+        currentIndex = 0;
+
+        for (int i = 0; i < availableHeroes.Length; i++)
+        {
+            availableHeroes[i].SetActive(false);
+        }
+        availableHeroes[currentIndex].SetActive(true);
+
+        //heroes = GameManager
+    }
+
+    public void NextHero()
+    {
+        availableHeroes[currentIndex].SetActive(false);
+
+        if (currentIndex + 1 == availableHeroes.Length)
+            currentIndex = 0;
+        else
+            currentIndex++;
+
+        availableHeroes[currentIndex].SetActive(true);
+    }
+
+    public void PreviousHero()
+    {
+        availableHeroes[currentIndex].SetActive(false);
+
+        if (currentIndex - 1 == -1)
+            currentIndex = availableHeroes.Length - 1;
+        else
+            currentIndex--;
+
+        availableHeroes[currentIndex].SetActive(true);
+    }
+}
---
+++ Awesome Road Runner/Assets/Scripts/Helper Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject heroMenu;
+    public Text starScoreText;
+    public Image musicImg;
+    public Sprite musicOff, musicOn;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HeroMenu()
+    {
+        heroMenu.SetActive(true);
+    }
+
+    public void HomeButton()
+    {
+        heroMenu.SetActive(false);
+    }
+}
